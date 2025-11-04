138c13fb4b8e6f95449303b066c9e1f427ed023a
apb-nscc <the.alchemist.coder@gmail.com>
2025-11-04 12:09:29 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,7 @@
+import http from 'http';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+  res.write('hello world');
+  res.end();
+}).listen(3000);
