cd24360fbb34032bdb229a837041a8f0f923228b
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-11-06 14:35:36 -0400

    Implemented ENV and Connected to Database

--- routes.js
+++ routes.js
@@ -1,22 +1,29 @@
 import express from 'express'
+import sql from 'mssql'
+import 'dotenv/config'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const occasions = [
-        { id: 1, description: 'Birthday' },
-        { id: 2, description: 'Anniversary' },
-        { id: 3, description: 'Graduation' }
-    ];
-    res.json(occasions);
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
+
+router.get('/', async (req, res) => {
+
+    await sql.connect(dbConnectionString);
+
+    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name from [dbo].[Occasion] a INNER JOIN [dbo].[Category] b ON a.CategoryId = b.CategoryId ORDER BY a.[CreatedAt] DESC`;
+    
+    console.dir(result);
+    res.json(result.recordset);
 })
 
-router.get('/:id', (req,res) => {
+router.get('/:id', async (req,res) => {
     const id = req.params.id;
-    res.send(`Occasion details for occasion id: ${id}`);
 
-    const occasion = { id: id, description: 'Sample Occasion' };
-    res.json(occasion);
+    await sql.connect(dbConnectionString);
+    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name from [dbo].[Occasion] a INNER JOIN [dbo].[Category] b ON a.CategoryId = b.CategoryId WHERE a.OccasionId = ${id}`;
+    
+    console.dir(result);
+    res.json(result.recordset);
     
 })
 
