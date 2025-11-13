3b1b9afaa4f01f4a176a35d22c67727a35e69d22
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-11-13 15:51:34 -0400

    Ready For Marking

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const dbConnectionString = process.env.DB_CONNECTION_STRING;
 
+//Get: /api/occasions
 router.get('/', async (req, res) => {
 
     await sql.connect(dbConnectionString);
@@ -20,6 +21,45 @@ router.get('/', async (req, res) => {
     res.json(result.recordset);
 })
 
+
+//Post: /api/occasions/comments
+router.post('/comments', async (req, res) => {
+    const comment = req.body;
+    await sql.connect(dbConnectionString);
+
+    const result = await sql.query`INSERT INTO [dbo].[Comment] (Body, Author, CreateDate, OccasionId) 
+        VALUES (${comment.body}, ${comment.author}, ${comment.createDate}, ${comment.occasionId})`;
+    
+    res.json({message: 'Comment created'});
+
+});
+
+//Post: /api/occasions/purchases
+router.post('/purchases', async (req, res) => {
+    const purchase = req.body;
+    await sql.connect(dbConnectionString);
+
+    const result = await sql.query`INSERT INTO [dbo].[Purchase] (NumTicketsOrdered, CustomerFirstName, CustomerLastName, CustomerEmail, CustomerPhone, CustomerAddress, CreditCardNumber, CreditCardExpiry, CreditCardCvv, PurchaseDate, OccasionId) 
+        VALUES (${purchase.numTicketsOrdered}, ${purchase.customerFirstName}, ${purchase.customerLastName}, ${purchase.customerEmail}, ${purchase.customerPhone}, ${purchase.customerAddress}, ${purchase.creditCardNumber}, ${purchase.creditCardExpiry}, ${purchase.creditCardCvv}, ${purchase.purchaseDate}, ${purchase.occasionId})`;
+    
+    res.json({message: 'Purchase created'});
+
+});
+
+//Get: /api/occasions/purchases
+router.get('/purchases', async (req, res) => {
+
+    await sql.connect(dbConnectionString);
+
+    const result = await sql.query`SELECT a.NumTicketsOrdered, a.CustomerFirstName, a.CustomerLastName, a.CustomerEmail, a.CustomerPhone, a.CustomerAddress, a.CreditCardNumber, a.CreditCardExpiry, a.CreditCardCvv, a.PurchaseDate, a.OccasionId 
+        FROM [dbo].[Purchase] a`;
+    
+    console.dir(result);
+    res.json(result.recordset);
+
+});
+
+//Get: /api/occasions/:id
 router.get('/:id', async (req,res) => {
     const id = req.params.id;
 
@@ -39,16 +79,5 @@ router.get('/:id', async (req,res) => {
     }    
 });
 
-//Post: /api/occasions
-router.post('/', async (req, res) => {
-    const occasion = req.body;
-    await sql.connect(dbConnectionString);
-
-    const result = await sql.query`INSERT INTO [dbo].[Comment] (Body, Author, CreateDate, OccasionId) 
-        VALUES (${occasion.body}, ${occasion.author}, ${occasion.createDate}, ${occasion.occasionId})`;
-    
-    res.json({message: 'Comment created'});
-
-});
 
 export default router;
