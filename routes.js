2ebacf358581d8c4d1d57a088bb4ba33672e49d5
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-11-13 12:52:09 -0400

    Update Route.js

--- routes.js
+++ routes.js
@@ -10,8 +10,12 @@ router.get('/', async (req, res) => {
 
     await sql.connect(dbConnectionString);
 
-    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name from [dbo].[Occasion] a INNER JOIN [dbo].[Category] b ON a.CategoryId = b.CategoryId ORDER BY a.[CreatedAt] DESC`;
-    
+    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name 
+        from [dbo].[Occasion] a 
+        INNER JOIN [dbo].[Category] b 
+        ON a.CategoryId = b.CategoryId 
+        ORDER BY a.[CreatedAt] DESC`;
+        
     console.dir(result);
     res.json(result.recordset);
 })
@@ -20,11 +24,31 @@ router.get('/:id', async (req,res) => {
     const id = req.params.id;
 
     await sql.connect(dbConnectionString);
-    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name from [dbo].[Occasion] a INNER JOIN [dbo].[Category] b ON a.CategoryId = b.CategoryId WHERE a.OccasionId = ${id}`;
-    
+    const result = await sql.query`SELECT a.OccasionId, a.Title, a.Description, a.Filename, a.CreatedAt, b.CategoryId, b.Name 
+        from [dbo].[Occasion] a 
+        INNER JOIN [dbo].[Category] b 
+        ON a.CategoryId = b.CategoryId 
+        WHERE a.OccasionId = ${id}`;
+        
     console.dir(result);
-    res.json(result.recordset);
+
+    if (result.recordset.length === 0) {
+        res.status(404).send('Occasion not found');
+    } else {
+        res.json(result.recordset);
+    }    
+});
+
+//Post: /api/occasions
+router.post('/', async (req, res) => {
+    const occasion = req.body;
+    await sql.connect(dbConnectionString);
+
+    const result = await sql.query`INSERT INTO [dbo].[Comment] (Body, Author, CreateDate, OccasionId) 
+        VALUES (${occasion.body}, ${occasion.author}, ${occasion.createDate}, ${occasion.occasionId})`;
     
-})
+    res.json({message: 'Comment created'});
+
+});
 
 export default router;
