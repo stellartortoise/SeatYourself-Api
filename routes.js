e9c8d94f83b3bd093f0b6c23bed984fb01b7e9a3
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-11-27 15:22:40 -0400

    Added Validation

--- routes.js
+++ routes.js
@@ -39,10 +39,66 @@ router.post('/purchases', async (req, res) => {
     const purchase = req.body;
     await sql.connect(dbConnectionString);
 
+
+    function Validate(status) {    
+    
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const expiryRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+
+        if (purchase.numTicketsOrdered <= 0) {
+            res.status(status).json({message: 'Invalid number of tickets ordered'});
+            return false;
+        }
+
+        if (!emailRegex.test(purchase.customerEmail)) {
+            res.status(status).json({message: 'Invalid customer email format'});
+            return false;
+        }
+
+        for (let letter in purchase.creditCardNumber) {
+            if (isNaN(purchase.creditCardNumber[letter])) {
+                res.status(status).json({message: 'Credit card number must be numeric'});
+                return false;
+            }
+        }
+
+        if (purchase.creditCardNumber.length !== 16) {
+            res.status(status).json({message: 'Invalid credit card number length'});
+            return false;
+        }
+
+        if (!expiryRegex.test(purchase.creditCardExpiry)) {
+            res.status(status).json({message: 'Invalid credit card expiry format'});
+            return false;
+        }
+
+        let ccvLength = 0;
+        for (let letter in purchase.creditCardCvv) {
+            if (isNaN(purchase.creditCardCvv[letter])) {
+                res.status(status).json({message: 'Credit card CVV must be numeric'});
+                return false;
+            }
+            ccvLength++;
+        }
+
+        if (ccvLength !== 3) {
+            res.status(status).json({message: 'Invalid credit card CVV length'});
+            return false;
+        }
+
+        return true;
+ 
+    }
+
+
+    if (Validate(400)) {
+
     const result = await sql.query`INSERT INTO [dbo].[Purchase] (NumTicketsOrdered, CustomerFirstName, CustomerLastName, CustomerEmail, CustomerPhone, CustomerAddress, CreditCardNumber, CreditCardExpiry, CreditCardCvv, PurchaseDate, OccasionId) 
         VALUES (${purchase.numTicketsOrdered}, ${purchase.customerFirstName}, ${purchase.customerLastName}, ${purchase.customerEmail}, ${purchase.customerPhone}, ${purchase.customerAddress}, ${purchase.creditCardNumber}, ${purchase.creditCardExpiry}, ${purchase.creditCardCvv}, ${purchase.purchaseDate}, ${purchase.occasionId})`;
+    }
     
     res.json({message: 'Purchase created'});
+
 
 });
 
